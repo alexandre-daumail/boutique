02c465b4b22cde0a8963047381c4b31a1278fdbf
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-02-24 14:20:36 +0100

    1RST

---
+++ Include/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/<?= $css ?>.css">
+    <title><?= $title ?></title>
+</head>
+
+<body>
+
+    <header>
+
+        <div class="left-header">
+
+            <img id="logo-header" src="assets/img/NOVA SHOP.png" alt="logo">
+
+        </div>
+        <div class="middle-header">
+
+            <div class="search-bar">
+                <form id="form">
+                    <input type="search" id="query" name="q" placeholder="Search...">
+                    <button>Search</button>
+                </form>
+            </div>
+
+            <div class="button">
+                <button class="">Accueil</button>
+                <button>Les Étoiles</button>
+                <button>Le Registre Internationnal</button>
+                <button>test</button>
+            </div>
+
+        </div>
+
+        <div class="right-header">
+            <img id="icon-profil" src="assets/img/icon/gg_profile.png" alt="icon-profil">
+            <img id="icon-bin" src="assets/img/icon/bin.png" alt="icon-bin">
+        </div>
+
+    </header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    session_start();
+    $title = "Accueil";
+    $css = "index";
+    require ('include/header.php'); 
+    
+?>
+<main>
+
+</main>
+
+<?php require ('include/footer.php'); ?>
