5e36dede2af9e9424dc8be7fdc4b16a6b695406d
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-02-24 20:47:11 +0100

    1rst commit essai nav

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/index.scss">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo-area">
+            <img src="assets/img/logo-header.png" alt="logo-header">
+        </div>
+        <div class="search-button-area">
+
+            <section class="search-area">
+                <form class="searchbox" action="http://thecodeblock.com">
+                    <input type="search" placeholder="Search" />
+                    <button type="submit" value="search">&nbsp;</button>
+                </form>
+            </section>
+            <section class="button-area">
+                <button class="header-button">Bouton</button>
+                <button class="header-button">Bouton</button>
+                <button class="header-button">Bouton</button>
+                <button class="button">
+                    <div class="button__content">
+                        <div class="button__icon">
+                            
+                                <g>
+                                    <path d="M64,48L64,48h-8V32h8c8.836,0,16-7.164,16-16S72.836,0,64,0c-8.837,0-16,7.164-16,16v8H32v-8c0-8.836-7.164-16-16-16
+          S0,7.164,0,16s7.164,16,16,16h8v16h-8l0,0l0,0C7.164,48,0,55.164,0,64s7.164,16,16,16c8.837,0,16-7.164,16-16l0,0v-8h16v7.98
+          c0,0.008-0.001,0.014-0.001,0.02c0,8.836,7.164,16,16,16s16-7.164,16-16S72.836,48.002,64,48z M64,8c4.418,0,8,3.582,8,8
+          s-3.582,8-8,8h-8v-8C56,11.582,59.582,8,64,8z M8,16c0-4.418,3.582-8,8-8s8,3.582,8,8v8h-8C11.582,24,8,20.417,8,16z M16,72
+          c-4.418,0-8-3.582-8-8s3.582-8,8-8l0,0h8v8C24,68.418,20.418,72,16,72z M32,48V32h16v16H32z M64,72c-4.418,0-8-3.582-8-8l0,0v-8
+          h7.999c4.418,0,8,3.582,8,8S68.418,72,64,72z" />
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                                </g>
+                                <g>
+                             
+                            </svg>
+                        </div>
+                    </div>
+                </button>
+
+                
+            </section>
+
+
+        </div>
+        <div class="icon-area">
+            <img id="icon-header" src="assets/img/icon-myaccount.png" alt="icon-myaccount">
+            <img id="icon-header" src="assets/img/icon-cart.png" alt="icon-cart">
+
+        </div>
+    </header>
+</body>
+
+</html>
