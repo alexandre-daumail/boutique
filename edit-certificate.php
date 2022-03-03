3dc9eecef58ab75c3ea05a3b7502289117b6c676
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-03-03 13:58:23 +0100

    edsit

--- edit-certificate.php
+++ edit-certificate.php
@@ -7,7 +7,16 @@ require('process/header.php');
 ?>
 
 <main>
-
+    <section class="grid">
+        <div class="item1">
+            <img src="public/img/CERTIFICAT.png" alt="">
+        </div>
+        <div class="item2">
+            <form action="#" method="POST">
+                <option value="1"><img src="public/img/icon/" alt=""></option>
+            </form>
+        </div>
+    </section>
 </main>
 
 <?php
