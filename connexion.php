a3a07e4c46aed9f59c0eff3bce22eff135cbc365
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-04-20 17:20:35 +0200

    update connexion.php

--- connexion.php
+++ connexion.php
@@ -25,11 +25,11 @@ if (isset($_POST['connect']) && count($_POST) > 0) {
 
             <label for="6">Identifiant</label>
 
-            <input type="text" name="login_connect" id="6" placeholder="Login ou Email" required>
+            <input id="6"vtype="text" name="login_connect" placeholder="Login ou Email" required>
 
             <label for="7">Mot de Passe</label>
 
-            <input type="password" name="pwd_connect" id="7" placeholder="Mot de passe" required>
+            <input id="7" type="password" name="pwd_connect" placeholder="Mot de passe" required>
 
             <button type="submit" name="connect" >Se connecter</button>
 
