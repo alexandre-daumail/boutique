99aaca31be65ab1cb9e34d18adb719ae6b1da331
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-04-21 11:02:07 +0200

    simplification foreach

--- classes/Category.php
+++ classes/Category.php
@@ -7,29 +7,24 @@ class Category extends Dbh
     public function categoryDropdown()
     {
         $query = $this->DbHandler()->prepare("SELECT name, id FROM category;");
-
         $query->execute();
-
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
         foreach ($result as $category) {
 
-                echo '<li class="dropdown"><a href="javascript:void(0)" class="dropbtn">' . $category["name"] . '</a><div class="dropdown-content">';
+            echo '<li class="dropdown"><a href="javascript:void(0)" class="dropbtn">' . $category["name"] . '</a><div class="dropdown-content">';
 
-                $query = $this->DbHandler()->prepare("SELECT name_sub FROM sub_category WHERE id_category = :id;");
+            $query = $this->DbHandler()->prepare("SELECT name_sub FROM sub_category WHERE id_category = :id;");
+            $query->execute([':id' => $category["id"]]);
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
-                $query->execute([':id' => $category["id"]]);
+            foreach ($result as $sub_category) {
 
-                $result = $query->fetchAll(PDO::FETCH_ASSOC);
+                echo '<a>' . $sub_category["name_sub"] . '</a>';
+                
+            }
 
-                foreach ($result as $sub_category) {
-
-                    foreach ($sub_category as $key => $value) {
-                        echo '<a>' . $value . '</a>';
-                    }
-                }
-
-                echo '</div></li>';
+            echo '</div></li>';
 
         }
     }
