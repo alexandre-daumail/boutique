5da9e4ccd84fb15fb053a5d6c2f903470c0ffb31
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-05-02 10:12:50 +0200

    Create checkout.php

---
+++ checkout.php
@@ -0,0 +1,18 @@
+<?php
+
+$title = "Passer commande";
+$css = "checkout";
+
+require('process/header.php');
+
+require('process/actionsPanier.php');
+
+?>
+
+
+
+<?php
+
+require('process/footer.php');
+
+?>
